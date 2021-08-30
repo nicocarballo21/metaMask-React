24ad098994968b0d400a25fddc064b5de5b36d80
nicocarballo21 <nlucas120@gmail.com>
2021-08-30 15:09:31 -0300

    first commit

---
+++ metamask/src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react"
+
+const App = () => {
+  return (
+    <div className="container">
+      <button>connect</button>
+    </div>
+  )
+}
+
+export default App
